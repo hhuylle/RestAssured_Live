b2158580e7676f93b6ff5a67a3bc1052c388be0b
mehmetCt <mehmet@cybertekschool.com>
2023-08-16 11:13:19 -0400

    OH --> HW1 Task 1 is started

---
+++ src/test/java/com/cydeo/officeHours/week1/morning/Homework1.java
@@ -0,0 +1,32 @@
+package com.cydeo.officeHours.week1.morning;
+
+import org.junit.jupiter.api.Test;
+
+public class Homework1 {
+
+
+
+    /*
+    Task 1 :
+    - Given accept type is Json
+    - When users sends request to /countries/US
+    - Then status code is 200
+    - And Content - Type is application/json
+    - And response contains United States of America
+     */
+
+    // ADD NEW TEST METHOD
+    /*
+        WIN -> ALT+INSERT
+        MAC -> CMD+N
+
+
+     */
+
+    @Test
+    public void task1() {
+
+
+
+    }
+}
