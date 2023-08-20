5bd5818ace90534072a52afed93e579c30b1e85d
mehmetCt <mehmet@cybertekschool.com>
2023-08-20 16:00:38 -0400

    liveClass - week2

---
+++ src/test/java/com/cydeo/liveClass/week2/P05_DeserializationPOJO.java
@@ -0,0 +1,22 @@
+package com.cydeo.liveClass.week2;
+
+import org.junit.jupiter.api.Test;
+
+public class P05_DeserializationPOJO {
+
+        /*
+        - ERGAST API
+        - Given accept type is json
+        - When user send request /status.json
+        - Then verify status code is 200
+        - And content type is application/json; charset=utf-8
+        - And total is 137
+        - And limit is 30
+        - And each status has statusId
+     */
+
+    @Test
+    public void statusPOJO() {
+
+    }
+}
