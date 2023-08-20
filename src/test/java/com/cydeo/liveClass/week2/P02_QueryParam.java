f0f4a8bebc4ead65b21bc92e71b8ff5292d8a7b0
mehmetCt <mehmet@cybertekschool.com>
2023-08-20 12:46:02 -0400

    liveClass - week2

--- src/test/java/com/cydeo/liveClass/week2/P02_QueryParam.java
+++ src/test/java/com/cydeo/liveClass/week2/P02_QueryParam.java
@@ -2,7 +2,11 @@ package com.cydeo.liveClass.week2;
 
 import com.cydeo.utility.FruitTestBase;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 public class P02_QueryParam extends FruitTestBase {
@@ -28,24 +32,27 @@ public class P02_QueryParam extends FruitTestBase {
     public  void getProducts() {
 
 
-
-        given().log().uri().accept(ContentType.JSON)
-                .queryParam("start",1)
-                .queryParam("limit",100)
-                .queryParam("search","Fruit").
-        when().get("/products").prettyPeek().
-        then()
+        JsonPath jsonPath = given().log().uri().accept(ContentType.JSON)
+                .queryParam("start", 1)
+                .queryParam("limit", 100)
+                .queryParam("search", "Fruit").
+                when().get("/products").prettyPeek().
+                then()
                 .statusCode(200)
                 .contentType(ContentType.JSON)
-                .body("meta.limit",is(100))
-                .body("meta.start",is(1))
-                .body("products.name",everyItem(containsString("Fruit")));
+                .body("meta.limit", is(100))
+                .body("meta.start", is(1))
+                .body("products.name", everyItem(containsString("Fruit")))
+                .extract().jsonPath();
 
 
         //     *     - Get all product names
+        List<String> allProductNames = jsonPath.getList("products.name");
+
         //     *     - Get product ids
+        List<Integer> allProductIDs = jsonPath.getList("products.id");
 
-
-
+        System.out.println(allProductNames);
+        System.out.println(allProductIDs);
     }
 }
