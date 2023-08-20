d5b65e66a2628f29f5e8bea2a17c474a2884b046
mehmetCt <mehmet@cybertekschool.com>
2023-08-20 16:50:38 -0400

    liveClass - week2

---
+++ src/test/java/com/cydeo/pojo/Status.java
@@ -0,0 +1,12 @@
+package com.cydeo.pojo;
+
+import lombok.Data;
+
+@Data
+public class Status {
+
+    private String statusId;
+    private String count;
+    private String status;
+
+}
--- src/test/java/com/cydeo/pojo/StatusTable.java
+++ src/test/java/com/cydeo/pojo/StatusTable.java
@@ -1,4 +1,13 @@
 package com.cydeo.pojo;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
 public class StatusTable {
+
+    @JsonProperty("Status")
+    private List<Status> statusList;
 }
