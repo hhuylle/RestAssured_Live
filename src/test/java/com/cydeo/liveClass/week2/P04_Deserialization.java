814a08be06899b2085ec80788a1fc1ab039b8189
mehmetCt <mehmet@cybertekschool.com>
2023-08-20 15:18:07 -0400

    liveClass - week2

--- src/test/java/com/cydeo/liveClass/week2/P04_Deserialization.java
+++ src/test/java/com/cydeo/liveClass/week2/P04_Deserialization.java
@@ -1,8 +1,14 @@
 package com.cydeo.liveClass.week2;
 
 import com.cydeo.utility.FruitTestBase;
+import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
+
+
+import java.util.Map;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
@@ -31,12 +37,31 @@ public class P04_Deserialization extends FruitTestBase {
     @Test
     public void getCustomers() {
 
-        given().log().uri().accept(ContentType.JSON).
-        when().get("/customers").prettyPeek().
-        then().statusCode(200)
-                .contentType(ContentType.JSON);
+        Response response = given().log().uri().accept(ContentType.JSON).
+                when().get("/customers").
+                then().statusCode(200)
+                .contentType(ContentType.JSON)
+                .extract().response();
+
+        JsonPath jsonPath = response.jsonPath();
+
+        Map<String, Object> allData = jsonPath.getMap("");
+        System.out.println(allData);
+
+        System.out.println("====== GET META ======");
+        Map<String, Integer> meta = (Map<String, Integer>) allData.get("meta");
+        System.out.println(meta);
 
 
+        System.out.println("====== GET LIMIT ======");
+
+        System.out.println("====== GET CUSTOMERS ======");
+
+        System.out.println("====== GET FIRST CUSTOMER ======");
+
+        System.out.println("====== PRINT CUSTOMERS IDs ======");
+
+        System.out.println("====== PRINT CUSTOMERS Names ======");
 
 
     }
