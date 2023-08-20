96e732b92cf1ef26d6c81e67eee95a7e8e90d64b
mehmetCt <mehmet@cybertekschool.com>
2023-08-20 14:54:11 -0400

    liveClass - week2

---
+++ src/test/java/com/cydeo/liveClass/week2/P04_Deserialization.java
@@ -0,0 +1,31 @@
+package com.cydeo.liveClass.week2;
+
+import com.cydeo.utility.FruitTestBase;
+import org.junit.jupiter.api.Test;
+
+public class P04_Deserialization extends FruitTestBase {
+    /**
+     * Send request to FruitAPI url and save the response
+     * Accept application/json
+     * GET /customers
+     * Store the response in Response Object that comes from get Request
+     * Print out followings
+     *     - Print response
+     *     - Content-Type is application/json
+     *     - Status Code is 200
+     *     - Retrieve data as JAVA Collections and print out following informations
+     *
+     System.out.println("====== GET META ======");
+     System.out.println("====== GET LIMIT ======");
+     System.out.println("====== GET CUSTOMERS ======");
+     System.out.println("====== GET FIRST CUSTOMER ======");
+     System.out.println("====== PRINT CUSTOMERS IDs ======");
+     System.out.println("====== PRINT CUSTOMERS Names ======");
+
+     *
+     */
+
+    @Test
+    public void getCustomers() {
+    }
+}
