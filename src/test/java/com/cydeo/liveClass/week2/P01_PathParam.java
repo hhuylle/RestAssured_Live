713f0f78cdb7dbdc9acea9e67b475c76e90d2301
mehmetCt <mehmet@cybertekschool.com>
2023-08-20 10:11:32 -0400

    liveClass - week2

---
+++ src/test/java/com/cydeo/liveClass/week2/P01_PathParam.java
@@ -0,0 +1,24 @@
+package com.cydeo.liveClass.week2;
+
+import org.junit.jupiter.api.Test;
+
+public class P01_PathParam {
+    /**
+     *1- Given accept type is Json
+     *2- Path Parameters value is
+     *     - id —> 8
+     *3- When user sends GET request to /products/{id}
+     *4- Verify followings
+     *     - Status code should be 200
+     *     - Content Type is application/json
+     *     - Print response
+     *     - id is 8
+     *     - Name is "Grapes"
+     *     - Vendor name is "Max Obsthof GmbH"
+     *
+     */
+    @Test
+    public void getSingleProduct() {
+
+    }
+}
