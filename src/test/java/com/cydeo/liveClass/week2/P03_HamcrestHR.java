cb5ae17a689777b7b309bdf5244e223a0d7ae89a
mehmetCt <mehmet@cybertekschool.com>
2023-08-20 14:48:49 -0400

    liveClass - week2

--- src/test/java/com/cydeo/liveClass/week2/P03_HamcrestHR.java
+++ src/test/java/com/cydeo/liveClass/week2/P03_HamcrestHR.java
@@ -2,8 +2,14 @@ package com.cydeo.liveClass.week2;
 
 import com.cydeo.utility.HrTestBase;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+
+import java.util.List;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
@@ -35,14 +41,48 @@ public class P03_HamcrestHR extends HrTestBase {
     @Test
     public void getCountries() {
 
-        given().log().uri().accept(ContentType.JSON)
-                .queryParam("q","{\"region_id\":2}").
-        when().get("/countries").prettyPeek().
-        then()
+        Response response = given().log().uri().accept(ContentType.JSON)
+                .queryParam("q", "{\"region_id\":2}").
+                when().get("/countries").prettyPeek().
+                then()
                 .statusCode(200)
                 .contentType(ContentType.JSON)
-                .header("Date",is(notNullValue()));
+                .header("Date", is(notNullValue()))
+                .body("count", is(5))
+                .body("hasMore", is(false))
+                .body("items[0].country_id", is("AR"))
+                .body("items.country_name", hasItem("Canada"))
+                .body("items.country_name", hasItems("Canada", "Mexico"))
+                .body("items", hasSize(5))
+                .body("items.country_id", everyItem(notNullValue()))
+                .body("items.region_id", everyItem(is(2)))
+                .extract().response();
 
+
+        JsonPath jsonPath = response.jsonPath();
+
+
+        //                   Verify
+        //     *            - count is 5
+        //     *            - hasMore is false
+        //     *            - first country id is AR
+        //     *            - country names have Canada
+        //     *            - country names have Canada,Mexico
+        //     *            - total country size is 5
+        //     *            - each country has country_id
+        //     *            - each country region_id is 2
+        //     *       - Print country names
+
+
+        // RESPONSE
+        List<String> allCountriesResponse = response.path("items.country_name");
+        System.out.println(allCountriesResponse);
+
+        // JSONPATH
+        List<String> allCountries = jsonPath.getList("items.country_name");
+        System.out.println(allCountries);
+
+        Assertions.assertEquals(5,allCountries.size());
 
     }
 }
